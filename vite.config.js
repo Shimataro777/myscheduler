0ab3d2e5aaaf9b7984506d50a8472081037e58f9
Shimataro777 <s4e.deut0605+github@gmail.com>
2026-08-29 20:36:12 +0900

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-/* base は "./" にしてある。
-   こうしておくと、GitHub Pages のどんな置き場所でも画像や部品を見失わない。 */
-export default defineConfig({
-  base: "./",
-  plugins: [react()],
-});
